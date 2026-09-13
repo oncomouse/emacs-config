1142ac1e7af1889922bad2ae1e9c86b2f382b060
oncomouse <oncomouse@gmail.com>
2026-09-13 12:29:40 -0500

    fix(popper): vc-git windows need to be managed by popper

--- init.el
+++ init.el
@@ -887,7 +887,7 @@ standard Emacs window‑selection utilities."
   (popper-group-function #'popper-group-by-project)
   (popper-reference-buffers
    '("\\*\\(lsp-help\\|Backtrace\\|Warnings\\|Compile-Log\\|[Hh]elp\\|Messages\\|Bookmark List\\|Occur\\|eldoc.*\\)\\*"
-	 "\\*\\(Flymake diagnostics\\|xref\\|ivy\\|Swiper\\|Completions\\|gptel-reasoning\\)"
+	 "\\*\\(Flymake diagnostics\\|xref\\|ivy\\|Swiper\\|Completions\\|gptel-reasoning\\|vc-git\\)"
 	 "Output\\*$"
 	 "\\*Async Shell Command\\*"
 	 help-mode
